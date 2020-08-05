fd6af96d8684ac7568d206f4e1efdfa30eac03ed
Rohit Saini <sketchlearn7@gmail.com>
2020-08-05 17:46:11 +0530

    Code Updated

--- SwiftUI25/ContentView.swift
+++ SwiftUI25/ContentView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var input: String = ""
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+        TextField("Enter...", text: $input).textFieldStyle(RoundedBorderTextFieldStyle())
+        if input != ""{
+            Image(uiImage: UIImage(data: generateQR(value: input))!).resizable().frame(width: 200,height: 200)
+        }
+        }.padding()
+    }
+    
+    func generateQR(value: String) -> Data{
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        let data = value.data(using: .ascii, allowLossyConversion: false)
+        filter?.setValue(data, forKey: "inputMessage")
+        let image = filter?.outputImage
+        let uiImage = UIImage(ciImage: image!)
+        return uiImage.pngData()!
     }
 }
 
